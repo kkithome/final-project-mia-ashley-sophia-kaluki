00c4d338e6fdb9a5fdc20bb4e015c3298974c56c
kkithome <kaluki_kithome@brown.edu>
2024-11-29 02:28:32 -0500

    did some file refactoring

--- activity-finder/client/src/geodata/TODO.md
+++
@@ -1,4 +0,0 @@
-TODO:
-download the geodata and place fullDownload.json into this directory here.
-
-<https://dsl.richmond.edu/panorama/redlining/data>
--- activity-finder/client/src/vite-env.d.ts
+++
@@ -1,8 +0,0 @@
-interface ImportMetaEnv {
-  VITE_APP_NODE_ENV: string;
-  // define more env variables if needed
-}
-
-interface ImportMeta {
-  env: ImportMetaEnv;
-}
