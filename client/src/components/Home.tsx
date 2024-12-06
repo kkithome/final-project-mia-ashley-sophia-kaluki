7b9980169fe272209483324ce13b1418f9551645
ashleywoertz <ashley.woertz@gmail.com>
2024-12-05 23:05:43 -0500

    home page general layout

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -5,6 +5,7 @@ import '../styles/index.css';
 import '../output.css';
 import Bear4 from '../assets/Bear4.png';
 import { useNavigate } from "react-router-dom";
+import Footer from "./Footer"; 
 
 export default function Home() {
   const navigate = useNavigate();
@@ -14,7 +15,7 @@ export default function Home() {
   };
 
   return (
-    <div className="space-y-10">
+    <div className="space-y-32 relative">
       <div className="flex flex-row items-end justify-center">
         <img
           src={Bear4}
@@ -27,9 +28,28 @@ export default function Home() {
         >
           Bear Tracks
         </p>
+      </div>
+
+      <div className="absolute top-[84px] left-1/2 transform -translate-x-1/2 bg-white rounded-full flex items-center justify-between px-2 py-2 md:px-2 md:py-2 w-2/3 lg:w-1/2 z-10">
+        <div className="flex-grow text-center kadwa text-customBrown text-md md:text-xl">
+          Location
+        </div>
+        <div className="flex-grow text-center kadwa text-customBrown text-md md:text-xl">
+          Date
+        </div>
+        <div className="flex-grow text-center kadwa text-customBrown text-md md:text-xl">
+          Add Filters
+        </div>
         <button onClick={handleSearchClick}
-          className="kadwa bg-customRed text-white rounded-lg text-md px-3 py-1 md:px-7 md:py-2 md:text-xl"
-        >Search</button>
+            className="paytone-one bg-customRed text-white rounded-full text-md px-2 py-1 md:px-4 md:py-2 md:text-xl"
+          >SEARCH</button>
+      </div>
+
+      <div className="px-8 py-12 bg-customLightBrown rounded-t-3xl">
+        <h1 className="kadwa text-customBrown text-3xl font-bold text-left mt-16 mb-10 ml-16">
+          Featured Activities
+        </h1>
+        {/* <FeaturedActivities /> */}
       </div>
     </div>
   );
