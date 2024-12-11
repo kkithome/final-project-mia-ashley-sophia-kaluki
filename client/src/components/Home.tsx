a9e7af12d346e344d312a3880969d262804deba0
ashleywoertz <ashley.woertz@gmail.com>
2024-12-11 17:27:23 -0500

    front-end assets + changes to Home front-end code

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -4,6 +4,9 @@ import '../styles/App.css';
 import '../styles/index.css';
 import '../output.css';
 import Bear4 from '../assets/Bear4.png';
+import DropDown from '../assets/FilterDropDown.png';
+import Pin from '../assets/BrownPin.png'; 
+import Calendar from '../assets/CalendarIcon.png'; 
 import { useNavigate } from "react-router-dom";
 import Footer from "./Footer"; 
 
@@ -31,9 +34,14 @@ export default function Home() {
       </div>
 
       <div className="absolute top-[84px] left-1/2 transform -translate-x-1/2 bg-white rounded-full flex items-center justify-between px-2 py-2 md:px-2 md:py-2 w-2/3 lg:w-1/2 z-10">
+        <img
+          src={Pin}
+          alt="A bear"
+          className="w-2 md:w-6 h-auto overflow-hidden relative"
+        />
         <div className="flex-grow text-center kadwa text-customBrown text-md md:text-xl">
           Location
-        </div>
+        </div> 
         <div className="flex-grow text-center kadwa text-customBrown text-md md:text-xl">
           Date
         </div>
@@ -50,6 +58,21 @@ export default function Home() {
           Featured Activities
         </h1>
         {/* <FeaturedActivities /> */}
+        <div className="flex flex-row md:flex-row items-center justify-center space-y-0 md:space-y-0 space-x-4 md:space-x-10">
+          {/*  items-center justify-center space-y-10 md:space-y-10 md:space-x-10 */}
+          <div className="bg-white rounded-2xl p-4 w-80 h-130 text-center space-y-2">
+            Test1
+          </div>
+          <div className="bg-white rounded-2xl p-4 w-80 h-130 text-center space-y-2">
+            Test2
+          </div>
+          <div className="bg-white rounded-2xl p-4 w-80 h-130 text-center space-y-2">
+            Test
+          </div>
+          <div className="bg-white rounded-2xl p-4 w-80 h-130 text-center space-y-2">
+            Test
+          </div>
+        </div>
       </div>
     </div>
   );
